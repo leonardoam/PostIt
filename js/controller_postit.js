2f255280a2ab56d58511a2f6781939e1a586c006
leonardoam <leonardoam94@gmail.com>
2016-04-02 02:10:49 -0300

    design adjust and pick an profile image (to be continued)

--- js/controller_postit.js
+++ js/controller_postit.js
@@ -6,6 +6,8 @@
     app.controller('Postit-Controller', ['$scope', function($scope) {
     	
     	$scope.signupPage = "signup.html";
+        $scope.pickImage = "pickimage.html";
+        $scope.profilePage = "profile.html";
     	    
       $scope.doSomething = function() {
       	console.log("Hi, I just did something!");
